4460971d3377b7275b1bcc27daaf50b8faead8aa
Eugene <laser4622>
2020-05-26 19:41:22 +0300

    fix

--- src/api/core/accessCode.js
+++ src/api/core/accessCode.js
@@ -2,9 +2,11 @@ const {Router} = require('express');
 
 const router = Router();
 
-router.get('/api/core/getDocument/', async (req, res)=>{
+router.get('/api/core/accessCode/', async (req, res)=>{
     try {
         const {code} = req.body;
+
+        console.log(code);
 
 
         return res.send('done');
