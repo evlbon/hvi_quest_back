a36d8d7602d17fbeac6b50fcf5d97ee3508068ea
Eugene <laser4622>
2020-05-26 20:03:54 +0300

    fix

--- src/api/core/accessCode.js
+++ src/api/core/accessCode.js
@@ -8,12 +8,17 @@ router.post('/api/core/accessCode/', async (req, res) => {
         const {code, redirectURL} = req.body;
         const clientId = '7484844';
         const clientSecret = '4IZoq9xSaqtmuycMBzd2';
-        const url = `https://oauth.vk.com/access_token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectURL}&code=${code}`;
+        let url = `https://oauth.vk.com/access_token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectURL}&code=${code}`;
 
         const response = (await axios.get(url)).data;
 
+        url =  `https://api.vk.com/method/users.get?user_ids=${response.user_id}&fields=bdate&access_token=${response.access_token}&v=5.107`
+        const vkUser = (await axios.get(url)).data;
 
-        console.log(response, req.body);
+
+
+
+        console.log(vkUser);
 
 
         return res.send('done');
